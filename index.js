822577726e6f34c476a3662bb5ae6dac6c3108e4
SiminAnaraki <simin.anaraki@gmail.com>
2024-09-06 19:46:02 +0100

    add second page

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 window.onscroll = function() {
     var header = document.querySelector('header');
-    var logo = document.querySelector('.logo');
+    var logo = document.querySelector('.sticky-logo') || document.querySelector('.logo');
     if (window.scrollY> 0) {
       header.classList.add('sticky-header');
-      
       logo.classList.add('sticky-logo');
       logo.classList.remove('logo');
     } else {
       header.classList.remove('sticky-header');
-      logo.classList.remove('sticky-logo');
       logo.classList.add('logo');
+      logo.classList.remove('sticky-logo');
+      
+      
     }
   };
