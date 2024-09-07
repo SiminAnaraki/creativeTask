8568160f6910f119b0dbc6c8b720f741746afe7f
SiminAnaraki <simin.anaraki@gmail.com>
2024-09-08 00:58:19 +0100

    add effects

--- index.js
+++ index.js
@@ -1,15 +1,120 @@
-window.onscroll = function() {
-    var header = document.querySelector('header');
-    var logo = document.querySelector('.sticky-logo') || document.querySelector('.logo');
-    if (window.scrollY> 0) {
-      header.classList.add('sticky-header');
-      logo.classList.add('sticky-logo');
-      logo.classList.remove('logo');
-    } else {
-      header.classList.remove('sticky-header');
-      logo.classList.add('logo');
-      logo.classList.remove('sticky-logo');
+const header = document.querySelector('header');
+const logo = document.querySelector('.dark-header') || document.querySelector('.logo');
+const navLinks = document.querySelectorAll('nav ul li a');
+const mega = document.querySelector('.mega-menu');
+const megaContent = document.querySelector('.mega-content-visible') || document.querySelector('.mega-content-hidden');
+const heroItems = document.querySelectorAll('.hero-item')
+const heroBars = document.querySelectorAll('.hero-bar')
+let isMegaActive = false;
+let currentHeroItem=0;
+
+document.addEventListener('DOMContentLoaded', function() {
+  const hamburger = document.querySelector('.hamburger-menu');
+  const hamburgerIcon = document.getElementById('hamburger-icon');
+  const navLinks = document.querySelector('.hamburger-menu ul');
+
+  hamburger.addEventListener('click', function() {
+      navLinks.classList.toggle('invisible');
       
-      
+      if (hamburgerIcon.classList.contains('fa-bars')) {
+          hamburgerIcon.classList.remove('fa-bars');
+          hamburgerIcon.classList.add('fa-times');
+      } else {
+          hamburgerIcon.classList.remove('fa-times');
+          hamburgerIcon.classList.add('fa-bars');
+      }
+  });
+});
+
+function toggleHeaderState(activate) {
+  if (activate) {
+    logo.classList.add('dark-header');
+    logo.classList.remove('logo');
+    navLinks.forEach(function(link) {
+      link.classList.add('dark-header');
+    });
+    if (!mega.classList.contains('active')) {
+      mega.classList.add('dark-services');
     }
-  };
+  } else {
+    logo.classList.add('logo');
+    logo.classList.remove('dark-header');
+    navLinks.forEach(function(link) {
+      link.classList.remove('dark-header');
+    });
+    if (!mega.classList.contains('active')) {
+      mega.classList.remove('dark-services');
+      mega.classList.remove('sticky');
+    }
+  }
+}
+
+function handleScroll() {
+  if (window.scrollY > 0) {
+    header.classList.add('sticky-header');
+    toggleHeaderState(true);
+  } else {
+    header.classList.remove('sticky-header');
+    toggleHeaderState(isMegaActive);
+  }
+}
+
+function toggleMegaMenu() {
+  isMegaActive = !isMegaActive;
+  
+  if (mega.classList.contains('dark-services')) {
+    mega.classList.replace('dark-services', 'active');
+    mega.classList.add('sticky');
+  } else if (mega.classList.contains('sticky')) {
+    mega.classList.toggle('active');
+  } else {
+    mega.classList.toggle('active');
+  }
+
+  if (megaContent.classList.contains('mega-content-hidden')) {
+    megaContent.classList.replace('mega-content-hidden', 'mega-content-visible');
+  } else {
+    megaContent.classList.replace('mega-content-visible', 'mega-content-hidden');
+  }
+
+  header.classList.toggle('light-header-bg');
+  
+  
+  if (window.scrollY === 0) {
+    toggleHeaderState(isMegaActive);
+  } else {
+    toggleHeaderState(true);
+  }
+}
+
+window.addEventListener('scroll', handleScroll);
+mega.addEventListener('click', toggleMegaMenu);
+
+
+
+
+
+
+function fadeHero() {
+  heroItems[currentHeroItem].classList.add('invisible');
+  currentHeroItem = (currentHeroItem + 1) % heroItems.length;
+  heroItems[currentHeroItem].classList.remove('invisible');
+}
+
+let fadeInterval = setInterval(fadeHero, 4000);
+
+
+
+
+heroBars.forEach(heroBar => {
+  heroBar.addEventListener('click', function() {
+      const dataIndex = heroBar.getAttribute('data-hero');
+      heroItems.forEach(item => item.classList.add('invisible'));
+      heroItems[dataIndex].classList.remove('invisible');
+      currentHeroItem = dataIndex;
+      clearInterval(fadeInterval);
+      fadeInterval = setInterval(fadeHero, 2000);
+  });
+});
+
+handleScroll();
